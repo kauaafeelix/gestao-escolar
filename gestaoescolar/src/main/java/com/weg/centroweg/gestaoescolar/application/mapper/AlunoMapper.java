c7c755b6596056ae3ba762a610e94f5fb090b2ca
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-26 16:40:01 -0300

    feat: adiciona mapper para transformar dto em entidade e vice-versa

--- gestaoescolar/src/main/java/com/weg/centroweg/gestaoescolar/application/mapper/AlunoMapper.java
+++ gestaoescolar/src/main/java/com/weg/centroweg/gestaoescolar/application/mapper/AlunoMapper.java
@@ -1,4 +1,31 @@
 package com.weg.centroweg.gestaoescolar.application.mapper;
 
+import com.weg.centroweg.gestaoescolar.application.dto.aluno.AlunoRequestDto;
+import com.weg.centroweg.gestaoescolar.application.dto.aluno.AlunoResponseDto;
+import com.weg.centroweg.gestaoescolar.domain.entity.Aluno;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AlunoMapper {
+
+    public Aluno toEntity (AlunoRequestDto alunoRequestDto){
+
+        return new Aluno(
+                alunoRequestDto.nome(),
+                alunoRequestDto.email(),
+                alunoRequestDto.matricula(),
+                alunoRequestDto.dataNascimento()
+        );
+    }
+
+    public AlunoResponseDto toDto (Aluno aluno){
+
+        return new AlunoResponseDto(
+                aluno.getId(),
+                aluno.getNome(),
+                aluno.getEmail(),
+                aluno.getMatricula(),
+                aluno.getDataNascimento()
+        );
+    }
 }
