d399f80d77b2d493127a92f4c95f5357ca7e68f4
Kaua Felix <kfelixcosta11@gmail.com>
2026-02-26 18:52:01 -0300

    delete

--- gestaoescolar/src/main/java/com/weg/centroweg/gestaoescolar/domain/repository/TurmaAlunoRepository.java
+++
@@ -1,4 +0,0 @@
-package com.weg.centroweg.gestaoescolar.domain.repository;
-
-public interface TurmaAlunoRepository {
-}
--- gestaoescolar/src/main/java/com/weg/centroweg/gestaoescolar/infra/persistence/TurmaAlunoRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.weg.centroweg.gestaoescolar.infra.persistence;
-
-import com.weg.centroweg.gestaoescolar.domain.repository.TurmaAlunoRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public class TurmaAlunoRepositoryImpl implements TurmaAlunoRepository {
-}
